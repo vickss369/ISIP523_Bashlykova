b5880b2b31b38330de5a3983f551eab068536ac4
vikab <vikabashlykova2007@gmail.com>
2025-10-25 16:00:04 +0300

    пример класса клиент

--- pr7/Program.cs
+++ pr7/Program.cs
@@ -4,13 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ISIP523_Bashlykova
+namespace pr7
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-
         }
     }
 }
