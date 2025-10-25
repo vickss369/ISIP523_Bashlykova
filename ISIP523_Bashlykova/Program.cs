82be07fba8b53fd417514758ffca01a58d4e29a2
vikab <vikabashlykova2007@gmail.com>
2025-10-25 16:21:07 +0300

    все классы, начало (декомпозиция немного есть)

--- ISIP523_Bashlykova/Program.cs
+++ ISIP523_Bashlykova/Program.cs
@@ -29,11 +29,71 @@ namespace ISIP523_Bashlykova
             public double price;
             public int quantity;
 
-            /*public bool IsAvailable()
+            /*public bool IsOnSklad()
             {
-               
             }*/
         }
+
+        class Sklad
+        {
+            public int id;
+            public List<Detail> allDetails = new List<Detail>();
+
+            /*public bool CheckDetailOnSklad(string partName)
+            {
+            }*/
+
+            /*public void AddDetail(Detail part)
+            {
+            }*/
+
+            /*public void TakeAndRemoveDetail(string partName, int quantity)
+            {
+            }*/
+        }
+
+        class RepairOrder
+        {
+            public int id;
+            public Client client;
+            public List<Detail> neededParts = new List<Detail>();
+            public double cost;
+            public string status;
+
+            /*public double CalculateRepairCost()
+            {
+            }*/
+        }
+
+        class AutoService
+        {
+            public int id;
+            public string name;
+            public double balance;
+            public Sklad sklad;
+            public List<RepairOrder> orders = new List<RepairOrder>();
+
+            /*public void TakeOrder(RepairOrder order)
+            {
+            }*/
+
+            /*public void RejectOrder(RepairOrder order)
+            {
+            }*/
+
+            /*public void RepairCar(RepairOrder order)
+            {
+            }*/
+
+            /*public void BuyDetails(Detail part, int quantity)
+            {
+            }*/
+
+            /*public void ShowInfo()
+            {
+            }*/
+        }
+
 
         static void Main(string[] args)
         {
