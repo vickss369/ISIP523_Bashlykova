57b7347fcbe75b10240949bac1387d941a25278b
vikab <vikabashlykova2007@gmail.com>
2025-10-26 15:21:04 +0300

    конструкторы создала для простоты и начала функции потихоньку

---
+++ ISIP523_Bashlykova/Core.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISIP523_Bashlykova
+{
+    internal class Core
+    {
+        public static AutoservicePR7Entities Context = new AutoservicePR7Entities();
+    }
+}
--- ISIP523_Bashlykova/Program.cs
+++ ISIP523_Bashlykova/Program.cs
@@ -13,13 +13,27 @@ namespace ISIP523_Bashlykova
             public int id;
             public string mark;
             public string problem;
+
+            public Car(int id, string mark, string problem)
+            {
+                this.id = id;      
+                this.mark = mark;
+                this.problem = problem;
+            }
         }
 
         class Client
         {
             public int id;
-            public string name;
+            public string fio;
             public Car car;
+
+            public Client(int id, string fio, Car car)
+            {
+                this.id = id;
+                this.fio = fio;
+                this.car = car;
+            }
         }
 
         class Detail
@@ -29,8 +43,20 @@ namespace ISIP523_Bashlykova
             public double price;
             public int quantity;
 
+            public Detail(int id, string name, double price, int quantity)
+            {
+                this.id = id;
+                this.name = name;
+                this.price = price;
+                this.quantity = quantity;
+            }
+
             /*public bool IsOnSklad()
             {
+                Console.Write("Введите название необходимой детали для проверки наличия на складе: ");
+                string nameNeededDetail = Console.ReadLine();
+                var detail = Core.Context.Detail.FirstOrDefault(d => d.Name == nameNeededDetail);
+                return detail != null && detail.QuantityOnSklad > 0;
             }*/
         }
 
@@ -39,9 +65,19 @@ namespace ISIP523_Bashlykova
             public int id;
             public List<Detail> allDetails = new List<Detail>();
 
-            /*public bool CheckDetailOnSklad(string partName)
+            public Sklad(int id, List<Detail> allDetails)
             {
-            }*/
+                this.id = id;
+                this.allDetails = allDetails;
+            }
+
+            public bool CheckDetailOnSklad(string partName)
+            {
+                Console.Write("Введите название необходимой детали для проверки наличия на складе: ");
+                string nameNeededDetail = Console.ReadLine();
+                var detail = Core.Context.Detail.FirstOrDefault(d => d.Name == nameNeededDetail);
+                return detail != null && detail.QuantityOnSklad > 0;
+            }
 
             /*public void AddDetail(Detail part)
             {
@@ -60,6 +96,15 @@ namespace ISIP523_Bashlykova
             public double cost;
             public string status;
 
+            public RepairOrder(int id, Client client, List<Detail> neededParts, double cost, string status)
+            {
+                this.id = id;
+                this.client = client;
+                this.neededParts = neededParts;
+                this.cost = cost;
+                this.status = status;
+            }
+
             /*public double CalculateRepairCost()
             {
             }*/
@@ -72,6 +117,7 @@ namespace ISIP523_Bashlykova
             public double balance;
             public Sklad sklad;
             public List<RepairOrder> orders = new List<RepairOrder>();
+
 
             /*public void TakeOrder(RepairOrder order)
             {
@@ -96,7 +142,8 @@ namespace ISIP523_Bashlykova
 
         static void Main(string[] args)
         {
-
-        }
+            }
     }
 }
+
+
